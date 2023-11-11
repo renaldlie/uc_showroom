db3ba5948e7dc4b34a7411a1e23cfe8a8284ccfb
renaldlie <74336039+renaldlie@users.noreply.github.com>
2023-11-11 13:09:10 +0800

    initial commit

---
+++ app/src/main/java/com/example/uc_showroom/DetailPesanan.kt
@@ -0,0 +1,11 @@
+package com.example.uc_showroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailPesanan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pesanan_detail)
+    }
+}
---
+++ app/src/main/java/com/example/uc_showroom/TambahPesanan.kt
@@ -0,0 +1,11 @@
+package com.example.uc_showroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TambahPesanan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambah_pesanan)
+    }
+}
